9f1e07eec377a174a6f188f4333261427e8913d0
sang14 <sangya.14@gmail.com>
2016-10-15 11:48:06 +0530

    another commit

--- myinfo/views.py
+++ myinfo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.conf import settings
 
-# Create your views here.
+
+def index(request):
+	context_dict = {}
+	context_dict['hi'] = 'hi'
+	
+	return render(request, 'index.html', context_dict)
